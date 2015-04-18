568a173ac690ed8a3f94aad079211292826da18f
Boris Cherny <bcherny@gmail.com>
2015-04-18 14:26:20 -0700

    begin work on oauth

--- www/dist/scripts/main.js
+++ www/dist/scripts/main.js
@@ -1,25 +1,94 @@
-var ____Class2=React.Component;for(var ____Class2____Key in ____Class2){if(____Class2.hasOwnProperty(____Class2____Key)){App[____Class2____Key]=____Class2[____Class2____Key];}}var ____SuperProtoOf____Class2=____Class2===null?null:____Class2.prototype;App.prototype=Object.create(____SuperProtoOf____Class2);App.prototype.constructor=App;App.__superConstructor__=____Class2;
+var ____Class2Y=React.Component;for(var ____Class2Y____Key in ____Class2Y){if(____Class2Y.hasOwnProperty(____Class2Y____Key)){App[____Class2Y____Key]=____Class2Y[____Class2Y____Key];}}var ____SuperProtoOf____Class2Y=____Class2Y===null?null:____Class2Y.prototype;App.prototype=Object.create(____SuperProtoOf____Class2Y);App.prototype.constructor=App;App.__superConstructor__=____Class2Y;
 
   function App(props) {"use strict";
-    ____Class2.call(this,props)
+    ____Class2Y.call(this,props)
     this.state = {}
   }
 
   Object.defineProperty(App.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
     return (
-      React.createElement("query-bar", null)
+      React.createElement("div", null, 
+        React.createElement(GithubLogin, null), 
+        React.createElement(QueryBar, null)
+      )
     )
   }});
 
 
 
 
-var ____Class3=React.Component;for(var ____Class3____Key in ____Class3){if(____Class3.hasOwnProperty(____Class3____Key)){QueryBar[____Class3____Key]=____Class3[____Class3____Key];}}var ____SuperProtoOf____Class3=____Class3===null?null:____Class3.prototype;QueryBar.prototype=Object.create(____SuperProtoOf____Class3);QueryBar.prototype.constructor=QueryBar;QueryBar.__superConstructor__=____Class3;
+var ____Class2Z=React.Component;for(var ____Class2Z____Key in ____Class2Z){if(____Class2Z.hasOwnProperty(____Class2Z____Key)){GithubLogin[____Class2Z____Key]=____Class2Z[____Class2Z____Key];}}var ____SuperProtoOf____Class2Z=____Class2Z===null?null:____Class2Z.prototype;GithubLogin.prototype=Object.create(____SuperProtoOf____Class2Z);GithubLogin.prototype.constructor=GithubLogin;GithubLogin.__superConstructor__=____Class2Z;
 
-  function QueryBar(props) {"use strict";
-    ____Class3.call(this,props)
+  function GithubLogin(props) {"use strict";
+    ____Class2Z.call(this,props)
+    this.state = {
+      githubOauthClientId: '5dda5e640b390bc40468',
+      githubOauthState: Math.random()*100000000000000000
+    }
   }
 
-  
+  Object.defineProperty(GithubLogin.prototype,"login",{writable:true,configurable:true,value:function() {"use strict";
+
+    const scope = ['user:email', 'read:org'].join(',')
+
+    window.location.href = ("https://github.com/login/oauth/authorize?client_id=" +  this.state.githubOauthClientId + "&state=" +  this.state.githubOauthState + "&scope=" +  scope)
+
+  }});
+
+  Object.defineProperty(GithubLogin.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
+
+    var code = null,
+        state = null
+
+    try {
+      code = window.location.search.match(/code=([^&]+)/)[1]
+      state = window.location.search.match(/state=([^&]+)/)[1]
+    } catch (e){}
+
+    console.log(code, state, this.state.githubOauthState)
+
+    if (code && state && !this.state.token) {
+
+      // if (state != this.state.githubOauthState) {
+      //   throw new Error ('states don\'t match, possible XSF attack. aborting!')
+      // }
+
+      $
+      .post(("https://github.com/login/oauth/access_token/client_id=" +  this.state.githubOauthClientId + "&client_secret=af9b23df713de6a5cfc819a92e0ae6f799a800b3&code=" +  code))
+      .then(function(data)  {
+       console.log(data.token)
+      })
+
+    }
+
+    var githubLogin = code
+      ? React.createElement("div", null, "logged in!")
+      : React.createElement("a", {href: "#", onClick: this.login.bind(this)}, "Sign into Github")
+
+    return (
+      githubLogin
+    )
+  }});
 
 
+
+var ____Class30=React.Component;for(var ____Class30____Key in ____Class30){if(____Class30.hasOwnProperty(____Class30____Key)){QueryBar[____Class30____Key]=____Class30[____Class30____Key];}}var ____SuperProtoOf____Class30=____Class30===null?null:____Class30.prototype;QueryBar.prototype=Object.create(____SuperProtoOf____Class30);QueryBar.prototype.constructor=QueryBar;QueryBar.__superConstructor__=____Class30;
+
+  function QueryBar(props) {"use strict";
+    ____Class30.call(this,props)
+    this.state = {}
+  }
+
+  Object.defineProperty(QueryBar.prototype,"render",{writable:true,configurable:true,value:function() {"use strict";
+    return (
+      React.createElement("input", {className: "QueryBar", type: "text"})
+    )
+  }});
+
+
+
+
+React.render(
+  React.createElement(App, null),
+  document.querySelector('#main')
+)
--- www/src/scripts/main.js
+++ www/src/scripts/main.js
@@ -7,10 +7,68 @@ class App extends React.Component {
 
   render() {
     return (
-      <query-bar></query-bar>
+      <div>
+        <GithubLogin></GithubLogin>
+        <QueryBar></QueryBar>
+      </div>
     )
   }
 
+
+}
+
+class GithubLogin extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      githubOauthClientId: '5dda5e640b390bc40468',
+      githubOauthState: Math.random()*100000000000000000
+    }
+  }
+
+  login() {
+
+    const scope = ['user:email', 'read:org'].join(',')
+
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${ this.state.githubOauthClientId }&state=${ this.state.githubOauthState }&scope=${ scope }`
+
+  }
+
+  render() {
+
+    var code = null,
+        state = null
+
+    try {
+      code = window.location.search.match(/code=([^&]+)/)[1]
+      state = window.location.search.match(/state=([^&]+)/)[1]
+    } catch (e){}
+
+    console.log(code, state, this.state.githubOauthState)
+
+    if (code && state && !this.state.token) {
+
+      // if (state != this.state.githubOauthState) {
+      //   throw new Error ('states don\'t match, possible XSF attack. aborting!')
+      // }
+
+      $
+      .post(`https://github.com/login/oauth/access_token/client_id=${ this.state.githubOauthClientId }&client_secret=af9b23df713de6a5cfc819a92e0ae6f799a800b3&code=${ code }`)
+      .then((data) => {
+       console.log(data.token)
+      })
+
+    }
+
+    var githubLogin = code
+      ? <div>logged in!</div>
+      : <a href="#" onClick={this.login.bind(this)}>Sign into Github</a>
+
+    return (
+      githubLogin
+    )
+  }
 
 }
 
@@ -18,9 +76,19 @@ class QueryBar extends React.Component {
 
   constructor (props) {
     super(props)
+    this.state = {}
   }
 
-  
+  render() {
+    return (
+      <input className="QueryBar" type="text" />
+    )
+  }
 
 
 }
+
+React.render(
+  <App />,
+  document.querySelector('#main')
+)
