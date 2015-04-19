007b1ce8813bd675fa9168d2d363a882dd45711f
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-04-18 20:00:02 -0700

    first cut at setting up couch for db

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-const PORT = 3000
+const PORT = 3000;    //TODO: This should be in config.json?
 
 const
+  cradle = require('cradle'),
   cookieParser = require('cookie-parser'),
   express = require('express'),
   https = require('https'),
   passport = require('passport'),
   session = require('express-session'),
-  GitHubStrategy = require('passport-github').Strategy
+  GitHubStrategy = require('passport-github').Strategy;
 
 // configure passport
 // @see https://github.com/jaredhanson/passport-github/blob/master/examples/login/app.js
@@ -16,18 +17,18 @@ passport.use(
     clientSecret: process.env.client_secret || 'af9b23df713de6a5cfc819a92e0ae6f799a800b3',
     callbackURL: 'http://localhost:3000/login/callback'
   }, function (accessToken, refreshToken, profile, done) {
-    console.info('got profile', profile)
+    console.info('got profile', profile);
     return done(null, profile)
   }
-))
+));
 
 passport.serializeUser(function(user, done) {
   done(null, user)
-})
+});
 
 passport.deserializeUser(function(obj, done) {
   done(null, obj)
-})
+});
 
 // configure express
 express()
@@ -37,7 +38,7 @@ express()
   .use(passport.session())
   .use(express.static(__dirname + '/../www/'))
   .get('/login', passport.authenticate('github'))
-  .get('/login/callback', 
+  .get('/login/callback',
     passport.authenticate('github', { failureRedirect: '/login' }),
     function(req, res) {
       // Successful authentication, redirect home.
@@ -53,7 +54,7 @@ express()
   .get('/user/orgs', function (req, res) {
     // TODO: this request should be authenticated with passport
     if (req.user) {
-      res.header('Content-Type', 'application/json')
+      res.header('Content-Type', 'application/json');
       https.get({
         headers: {
           'User-Agent': 'Kittens' // @see https://developer.github.com/v3/#user-agent-required
@@ -68,5 +69,13 @@ express()
     }
   })
   .listen(PORT, function () {
+    //TODO: Use a config library
+    cradle.setup({
+      host: '127.0.0.1:5984',
+      cache: true,
+      raw: false,
+      forceSave: true
+    });
+    //TODO (bcherny) help me get this line to work: new Db(new (cradle.connection), 'test')
     console.info('HTTP server listening on', PORT, '...')
-  })
+  });
