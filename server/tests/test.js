27f59ae4053b5342649aaf2896f65b7af6ab732e
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-04-22 21:02:37 -0700

    test eval db query on circleci

--- server/tests/test.js
+++ server/tests/test.js
@@ -5,7 +5,25 @@ const
 
 test(app)
   .get('/cow')
-  .expect(200, {"says": "moo"})
+  .expect(200, {"says": 'moo'})
   .end(function(err, res){
+    if (err) throw err;
+  });
+
+test(app)
+  .post('/eval/db/mysql')
+  .set('Content-Type', 'application/json')
+  .send({
+    "settings": {
+      "host": 'localhost',
+      "port": 3306,
+      "database": 'circle_test',
+      "username": 'ubuntu'
+    },
+    "query": 'SELECT * FROM users'
+  })
+  .expect(200)
+  .end(function (err, res) {
+    console.log(err || res);
     if (err) throw err;
   });
