696c0a320cd6203cd738a2315b37402604ee9fee
Boris Cherny <bcherny@gmail.com>
2015-05-10 14:15:14 -0700

    posting queries - working, but no settings yet

--- www/src/scripts/QueryBar.js
+++ www/src/scripts/QueryBar.js
@@ -1,4 +1,8 @@
+import { toArray } from 'lodash'
 import React from 'react'
+import CodeMirror from 'codemirror'
+import Sql from 'codemirror/mode/sql/sql'
+import request from 'superagent'
 
 export default class QueryBar extends React.Component {
 
@@ -7,9 +11,43 @@ export default class QueryBar extends React.Component {
     this.state = {}
   }
 
+  submit (query: string) {
+    console.log('submit', query)
+
+    request
+      .post('/eval')
+      .send({
+        query: query
+      })
+      .end((err, res) => {
+        if (err) throw err
+        console.log('response!', res)
+      })
+  }
+
+  componentDidUpdate() {
+
+    // TODO: figure out a better way to clean up old codemirror instances
+    toArray(document.querySelectorAll('.CodeMirror'))
+      .forEach(e => e.parentNode.removeChild(e))
+
+    let editor = CodeMirror
+      .fromTextArea(document.querySelector('.QueryBar'), {
+        autofocus: true,
+        extraKeys: {
+          'Cmd-Enter': () => {
+            this.submit(editor.getValue())
+          }
+        },
+        mode: 'sql',
+        theme: 'monokai'
+      })
+
+  }
+
   render() {
     return (
-      <input className="QueryBar" placeholder="Enter a query..." type="text" tabindex="0" />
+      <textarea className="QueryBar" placeholder="Enter a query..." tabindex="0"></textarea>
     )
   }
 
