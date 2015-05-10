fd7b7179cb707ae89ae9dccc4db7b21efffcd0c8
Boris Cherny <bcherny@gmail.com>
2015-05-10 12:57:09 -0700

    fix login ui

--- www/src/scripts/GithubLogin.js
+++ www/src/scripts/GithubLogin.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { giver } from './App'
 
 export default class GithubLogin extends React.Component {
 
   constructor (_) {
     super(_)
+    this.state = {}
+  }
+
+  componentDidMount() {
+
+    giver
+      .askFor('user')
+      .then(user => this.setState({ user: user }))
+
   }
 
   login() {
@@ -12,12 +22,12 @@ export default class GithubLogin extends React.Component {
 
   render() {
 
-    if (this.props.isLoggingIn) {
-      return <div>Signing in...</div>
+    if (this.props.isLoggingIn && !this.state.user) {
+      return <div className="GithubLogin">Signing in...</div>
     }
 
-    var githubLogin = this.props.user
-      ? <div>Signed in as <strong>{ this.props.user.username }</strong></div>
+    var githubLogin = this.state.user
+      ? <div>Signed in as <strong>{ this.state.user.username }</strong></div>
       : <a onClick={this.login.bind(this)}>Sign into Github</a>
 
     return (
